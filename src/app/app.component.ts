e545f4cfa99151efec76c23e9c0b5249e1156d88
Will <bgigs37@gmail.com>
2021-11-07 23:29:52 -0500

    inprogress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'personal-website';
+  public readonly title = 'Personal Website';
+  public readonly height = 100;
+  public readonly width = 100;
+  public readonly name = new Audio('../assets/voice.mp3')
+  public myStyle = {
+    position: 'fixed',
+    width: '100%',
+    height: '100%',
+    'z-index': -1,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  };
+
+  public myParams = {
+    particles: {
+      number: {
+        value: 200,
+      },
+      color: {
+        value: '#6699cc',
+      },
+      shape: {
+        type: 'triangle',
+      },
+    },
+  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ParticlesModule } from 'angular-particle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from '@angular/material/icon'
+import { NavbarModule, WavesModule, ButtonsModule } from 'ng-uikit-pro-standard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ParticlesModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    NavbarModule,
+    WavesModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
